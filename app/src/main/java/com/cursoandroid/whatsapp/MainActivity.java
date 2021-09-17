95188e16e8ef931b105a35f8f7ce9998aa311200
Leonardo Moreira <lmoreira597@gmail.com>
2021-09-16 23:52:06 -0300

    inicio

---
+++ app/src/main/java/com/cursoandroid/whatsapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.cursoandroid.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    //private DatabaseReference referencia_firebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     //   referencia_firebase.child("pontos").setValue(100);
+    }
+}
